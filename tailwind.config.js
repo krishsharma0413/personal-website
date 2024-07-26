aa70b0bf599c6e3b26c11a046ee19ef50ed053fc
krishsharma0413 <krishsharma0413@gmail.com>
2024-07-27 01:23:43 +0530

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
+      colors:{
+        "bg": "#000000",
+        "text": "#ffffff"
+      },
       fontFamily: {
-        'the-bomb-sound': ['the-bomb-sound'],
-        'galiver': ['galiver'],
+        "betaron": "Betaron Regular",
+        "neuebold": "Neue Bold",
+        "neueroman": "Neue Roman",
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, rgba(237, 128, 71, 1) 0%, rgba(225, 103, 98, 1) 28%, rgba(184, 100, 149, 1) 69%, rgba(63, 117, 233, 1) 100%)',
+      },
+      gridTemplateColumns: {
+        'auto-fill-100': 'repeat(auto-fill, minmax(100px, 1fr))',
+        'auto-fit-100': 'repeat(auto-fit, minmax(100px, 1fr))',
       },
     },
   },
